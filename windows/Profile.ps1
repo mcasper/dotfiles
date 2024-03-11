f23877cf920fdc9604bf2ce25ad904e7318e1887
Matt Casper <matt@tuple.app>
2024-03-11 08:15:07 -0700

    Powershell profile

--- windows/Profile.ps1
+++ windows/Profile.ps1
@@ -6,7 +6,7 @@ function Prompt {
     $END = "[0m"
     $path = (Get-Location).path.split("\\")[-3..-1] -join "\"
     $branch = $(git rev-parse --abbrev-ref HEAD)
-    if ($branch.length > 30) {
+    if ($branch.length -gt 30) {
         $branch = $branch.substring(0,30)
     }
     $branch_string = " (${ESC}${GREEN}${branch}${ESC}${END})"
@@ -18,3 +18,13 @@ function Prompt {
 
 # Add custom tools/aliases to the path
 $env:Path += ";${HOME}\dotfiles\windows\aliases"
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
