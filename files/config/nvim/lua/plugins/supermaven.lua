ad6ce15dbf2bb295f0dafed94371e253e2d7ab2f
Matt Casper <matthewvcasper@gmail.com>
2024-10-02 08:10:31 -0700

    Roll supermaven back to stable version

--- files/config/nvim/lua/plugins/supermaven.lua
+++ files/config/nvim/lua/plugins/supermaven.lua
@@ -1,6 +1,7 @@
 return {
     {
       "supermaven-inc/supermaven-nvim",
+      commit = "40bde487fe31723cdd180843b182f70c6a991226",
       config = function()
         require("supermaven-nvim").setup({})
       end,
