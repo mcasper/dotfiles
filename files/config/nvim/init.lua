8cccf9178225e3f799c9d960081dd3eb3f41f5c9
Matt Casper <matthewvcasper@gmail.com>
2025-09-25 18:30:40 -0700

    Lua formatting

--- files/config/nvim/init.lua
+++ files/config/nvim/init.lua
@@ -7,21 +7,21 @@ vim.g.maplocalleader = "\\"
 
 -- Mapping helpers
 function map(mode, shortcut, command)
-    vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
+	vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
 end
 
 function nmap(shortcut, command)
-    map("n", shortcut, command)
+	map("n", shortcut, command)
 end
 
 function vmap(shortcut, command)
-    map("v", shortcut, command)
+	map("v", shortcut, command)
 end
 
 vim.cmd.filetype({ "plugin", "indent", "on" })
 
 -- Options
-vim.opt.relativenumber = true;
+vim.opt.relativenumber = true
 vim.opt.wildmenu = true
 vim.opt.backspace = "indent,eol,start"
 vim.opt.tabstop = 2
@@ -90,8 +90,8 @@ nmap("<Leader>le", ":%s/\\r$//<CR>")
 nmap("<Leader>hs", ":s/:\\([^ ]*\\)\\(\\s*\\)=>/\1:/g<CR>")
 nmap("<Leader>i", ":lua CorrectIndentation()<CR>")
 nmap("<Leader>n", ":lua RenameFile()<CR>")
-nmap("<Leader>p", ":call AddDebugger(\"o\")<CR>")
-nmap("<Leader>P", ":call AddDebugger(\"O\")<CR>")
+nmap("<Leader>p", ':call AddDebugger("o")<CR>')
+nmap("<Leader>P", ':call AddDebugger("O")<CR>')
 nmap("<Leader>d", ":call RemoveAllDebuggers()<CR>")
 
 nmap("<Leader>a", "<cmd>ArgWrap<CR>")
@@ -101,7 +101,7 @@ nmap("aa", ":call CocAction('diagnosticNext')<cr>")
 nmap("<Leader>s", ":TestNearest<CR>")
 nmap("<Leader>r", ":TestFile<CR>")
 
-nmap("<C-\\>", ":tab split<CR>:exec(\"tag \".expand(\"<cword>\"))<CR>")
+nmap("<C-\\>", ':tab split<CR>:exec("tag ".expand("<cword>"))<CR>')
 vmap("<Enter>", "<Plug>(EasyAlign)")
 nmap("k", "gk")
 nmap("j", "gj")
@@ -128,16 +128,16 @@ vim.cmd([[
 ]])
 
 function CorrectIndentation()
-  vim.cmd([[silent normal! gg=G]])
+	vim.cmd([[silent normal! gg=G]])
 end
 
 function RenameFile()
-  local old_name = vim.fn.expand('%')
-  local new_name = vim.fn.input('New file name: ', old_name, 'file')
-  if new_name ~= '' and new_name ~= old_name then
-    vim.cmd('saveas ' .. new_name)
-    vim.cmd('silent !rm ' .. old_name)
-  end
+	local old_name = vim.fn.expand("%")
+	local new_name = vim.fn.input("New file name: ", old_name, "file")
+	if new_name ~= "" and new_name ~= old_name then
+		vim.cmd("saveas " .. new_name)
+		vim.cmd("silent !rm " .. old_name)
+	end
 end
 
 -- Plugins
@@ -145,187 +145,187 @@ end
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- Arg (un)wrapping
-    {
-      "FooSoft/vim-argwrap",
-    },
+	spec = {
+		-- Arg (un)wrapping
+		{
+			"FooSoft/vim-argwrap",
+		},
 
-    -- Formatting
-    {
-      "stevearc/conform.nvim",
-      config = function()
-        -- Command to enable/disable autoformat-on-save
-        vim.api.nvim_create_user_command("FormatDisable", function(args)
-          if args.bang then
-            -- FormatDisable! will disable formatting just for this buffer
-            vim.b.disable_autoformat = true
-          else
-            vim.g.disable_autoformat = true
-          end
-        end, {
-          desc = "Disable autoformat-on-save",
-          bang = true,
-        })
-        vim.api.nvim_create_user_command("FormatEnable", function()
-          vim.b.disable_autoformat = false
-          vim.g.disable_autoformat = false
-        end, {
-          desc = "Re-enable autoformat-on-save",
-        })
+		-- Formatting
+		{
+			"stevearc/conform.nvim",
+			config = function()
+				-- Command to enable/disable autoformat-on-save
+				vim.api.nvim_create_user_command("FormatDisable", function(args)
+					if args.bang then
+						-- FormatDisable! will disable formatting just for this buffer
+						vim.b.disable_autoformat = true
+					else
+						vim.g.disable_autoformat = true
+					end
+				end, {
+					desc = "Disable autoformat-on-save",
+					bang = true,
+				})
+				vim.api.nvim_create_user_command("FormatEnable", function()
+					vim.b.disable_autoformat = false
+					vim.g.disable_autoformat = false
+				end, {
+					desc = "Re-enable autoformat-on-save",
+				})
 
-        require("conform").setup({
-          formatters_by_ft = {
-            eruby = { "erb_format", "rustywind" },
-            javascript = { "biome", "biome-check", "biome-organize-imports", "rustywind" },
-            lua = { "stylua" },
-            python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-            -- ruby = { "syntax_tree", " rubocop" },
-            rust = { "rustfmt", lsp_format = "fallback" },
-            toml = { "taplo" },
-            typescript = { "biome", "biome-check", "biome-organize-imports", "rustywind" },
-            typescriptreact = { "biome", "biome-check", "biome-organize-imports", "rustywind" },
-          },
-          format_on_save = function(bufnr)
-            -- Disable with a global or buffer-local variable
-            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-              return
-            end
-            return { timeout_ms = 500, lsp_format = "fallback" }
-          end,
-        })
-      end,
-    },
+				require("conform").setup({
+					formatters_by_ft = {
+						eruby = { "erb_format", "rustywind" },
+						javascript = { "biome", "biome-check", "biome-organize-imports", "rustywind" },
+						lua = { "stylua" },
+						python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+						-- ruby = { "syntax_tree", " rubocop" },
+						rust = { "rustfmt", lsp_format = "fallback" },
+						toml = { "taplo" },
+						typescript = { "biome", "biome-check", "biome-organize-imports", "rustywind" },
+						typescriptreact = { "biome", "biome-check", "biome-organize-imports", "rustywind" },
+					},
+					format_on_save = function(bufnr)
+						-- Disable with a global or buffer-local variable
+						if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+							return
+						end
+						return { timeout_ms = 500, lsp_format = "fallback" }
+					end,
+				})
+			end,
+		},
 
-    -- Git
-    {
-      "tpope/vim-fugitive"
-    },
+		-- Git
+		{
+			"tpope/vim-fugitive",
+		},
 
-    -- Fuzzy search
-    {
-        "junegunn/fzf",
-    },
-    {
-        "junegunn/fzf.vim",
-    },
+		-- Fuzzy search
+		{
+			"junegunn/fzf",
+		},
+		{
+			"junegunn/fzf.vim",
+		},
 
-    -- Golang
-    {
-      "fatih/vim-go"
-    },
+		-- Golang
+		{
+			"fatih/vim-go",
+		},
 
-    -- Terraform/Packer
-    {
-      "jvirtanen/vim-hcl"
-    },
+		-- Terraform/Packer
+		{
+			"jvirtanen/vim-hcl",
+		},
 
-    -- Breakpoints
-    {
-      "mcasper/vim-infer-debugger"
-    },
+		-- Breakpoints
+		{
+			"mcasper/vim-infer-debugger",
+		},
 
-    -- LSP
-    {
-      "neovim/nvim-lspconfig",
-      config = function()
-        vim.lsp.enable("ruby_lsp")
-        vim.lsp.enable("ts_ls")
-        vim.lsp.enable("tailwindcss")
-        vim.lsp.enable("basedpyright")
+		-- LSP
+		{
+			"neovim/nvim-lspconfig",
+			config = function()
+				vim.lsp.enable("ruby_lsp")
+				vim.lsp.enable("ts_ls")
+				vim.lsp.enable("tailwindcss")
+				vim.lsp.enable("basedpyright")
 
-        vim.diagnostic.config({
-          virtual_lines = {
-            current_line = true,
-          },
-        })
-      end,
-    },
+				vim.diagnostic.config({
+					virtual_lines = {
+						current_line = true,
+					},
+				})
+			end,
+		},
 
-    -- Mkdir
-    {
-      "pbrisbin/vim-mkdir"
-    },
+		-- Mkdir
+		{
+			"pbrisbin/vim-mkdir",
+		},
 
-    -- Colors
-    {
-        "junegunn/seoul256.vim",
-        lazy = false,
-        config = function()
-            -- TODO: light mode
-            -- vim.cmd([[let g:seoul256_background = 255]])
-            -- vim.cmd([[colo seoul256-light]])
-            -- vim.cmd([[set background=light]])
+		-- Colors
+		{
+			"junegunn/seoul256.vim",
+			lazy = false,
+			config = function()
+				-- TODO: light mode
+				-- vim.cmd([[let g:seoul256_background = 255]])
+				-- vim.cmd([[colo seoul256-light]])
+				-- vim.cmd([[set background=light]])
 
-            vim.cmd([[let g:seoul256_background = 234]])
-            vim.cmd([[colo seoul256]])
-            vim.cmd([[set background=dark]])
-        end,
-    },
+				vim.cmd([[let g:seoul256_background = 234]])
+				vim.cmd([[colo seoul256]])
+				vim.cmd([[set background=dark]])
+			end,
+		},
 
-    -- Autocomplete
-    {
-        {
-          "supermaven-inc/supermaven-nvim",
-          commit = "40bde487fe31723cdd180843b182f70c6a991226",
-          config = function()
-            require("supermaven-nvim").setup({})
-          end,
-        }
-    },
+		-- Autocomplete
+		{
+			{
+				"supermaven-inc/supermaven-nvim",
+				commit = "40bde487fe31723cdd180843b182f70c6a991226",
+				config = function()
+					require("supermaven-nvim").setup({})
+				end,
+			},
+		},
 
-    -- Tests
-    {
-        "vim-test/vim-test",
-    },
+		-- Tests
+		{
+			"vim-test/vim-test",
+		},
 
-    -- Treesitter
-    {
-      "nvim-treesitter/nvim-treesitter",
-      run = ":TSUpdate",
-      event = "BufRead",
-      config = function()
-        local configs = require("nvim-treesitter.configs")
+		-- Treesitter
+		{
+			"nvim-treesitter/nvim-treesitter",
+			run = ":TSUpdate",
+			event = "BufRead",
+			config = function()
+				local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-          ensure_installed = {
-            "lua",
-            "javascript",
-            "html",
-            "css",
-            "typescript",
-            "tsx",
-            "ruby",
-          },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-      end
-    },
+				configs.setup({
+					ensure_installed = {
+						"lua",
+						"javascript",
+						"html",
+						"css",
+						"typescript",
+						"tsx",
+						"ruby",
+					},
+					sync_install = false,
+					highlight = { enable = true },
+					indent = { enable = true },
+				})
+			end,
+		},
 
-    -- Terraform again?
-    {
-      'hashivim/vim-terraform'
-    }
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+		-- Terraform again?
+		{
+			"hashivim/vim-terraform",
+		},
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
 })
